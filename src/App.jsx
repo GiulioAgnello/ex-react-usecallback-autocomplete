fcce64aaf5195148660c30782ab2d9b6d0da603f
agnellogiulio4@gmail.com <agnellogiulio4@gmail.com>
2025-09-01 12:06:17 +0200

    debounceImplement

--- src/App.jsx
+++ src/App.jsx
@@ -1,70 +1,94 @@
-import { useEffect, useState } from "react";
+// Funzione di Debounce
+const debounce = (callback, delay) => {
+  let timeout;
+  return (value) => {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => {
+      callback(value);
+    }, delay);
+  };
+};
+
+import { useCallback, useEffect, useState } from "react";
 
 export default function App() {
   const [products, setProduct] = useState([]);
+  const [query, setQuery] = useState("");
+
+  const fetchProduct = async (query) => {
+    try {
+      const res = await fetch(
+        `http://localhost:3333/products?search=${encodeURIComponent(query)}`
+      );
+      const data = await res.json();
+      setProduct(data);
+      console.log("api");
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const debounceFetchproduct = useCallback(debounce(fetchProduct, 500), []);
 
   useEffect(() => {
-    fetch(`http://localhost:3333/products`)
-      .then((res) => res.json())
-      .then((data) => setProduct(data))
-      .catch((err) => console.error(err));
-  }, []);
-
-  console.log(products);
+    debounceFetchproduct(query), [query];
+  });
 
   return (
     <>
       <div className="container-sm mt-5"></div>
       <div className="text-center mt-5 ">
         <label className="px-5" htmlFor="name">
-          Trova quello che cerchi --->
+          Trova quello che cerchi
         </label>
         <input
           type="text"
-          value="name"
-          onChange={() => {}}
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
           placeholder="cerca "
         />
       </div>
 
       <div className="row g-2">
-        {products.map((product) => {
-          const {
-            id,
-            image,
-            color,
-            brand,
-            connectivity,
-            description,
-            name,
-            price,
-            rating,
-            wireless,
-          } = product;
-          return (
-            <div key={id} className="col-4">
-              <div className="card">
-                <img src={image} className="card-img-top" alt={brand} />
-                <div className="card-body">
-                  <h5 className="card-title">{brand}</h5>
-                  <hr />
-                  <small>Color : {color}</small>
-                  <hr />
-                  <small>connectivity: {connectivity}</small>
-                  <p>description: {description}</p>
-                  <hr />
-                  <p>name: {name}</p>
-                  <hr />
-                  <p>price: {price}</p>
-                  <hr />
-                  <p>rating: {rating}</p>
-                  <hr />
-                  <p>wireless: {wireless}</p>
+        {query
+          ? products.map((product) => {
+              const {
+                id,
+                image,
+                color,
+                brand,
+                connectivity,
+                description,
+                name,
+                price,
+                rating,
+                wireless,
+              } = product;
+              return (
+                <div key={id} className="col-4">
+                  <div className="card">
+                    <img src={image} className="card-img-top" alt={brand} />
+                    <div className="card-body">
+                      <h5 className="card-title">{brand}</h5>
+                      <hr />
+                      <small>Color : {color}</small>
+                      <hr />
+                      <small>connectivity: {connectivity}</small>
+                      <p>description: {description}</p>
+                      <hr />
+                      <p>name: {name}</p>
+                      <hr />
+                      <p>price: {price}</p>
+                      <hr />
+                      <p>rating: {rating}</p>
+                      <hr />
+                      <p>wireless: {wireless}</p>
+                    </div>
+                  </div>
                 </div>
-              </div>
-            </div>
-          );
-        })}
+              );
+            })
+          : null}
       </div>
     </>
   );
